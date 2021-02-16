58ca5f9dac4d3c8cf54d6ba9e70fda97bada455d
Alexander <a.sribnyak8987@mail.ru>
2021-02-16 20:46:22 +0300

    Add mic permission

--- app/src/main/java/com/sribnyak/thesound/MainActivity.kt
+++ app/src/main/java/com/sribnyak/thesound/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.sribnyak.thesound
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.textView).text = "Permission no"
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                MIC_PERMISSION_CODE
+            )
+        } else onMicPermissionGranted()
     }
+
+    private fun onMicPermissionGranted() {
+        findViewById<TextView>(R.id.textView).text = "Permission yes"
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == MIC_PERMISSION_CODE && grantResults.isNotEmpty() &&
+            grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            onMicPermissionGranted()
+    }
+
+    companion object { private const val MIC_PERMISSION_CODE = 20567 }
 }
