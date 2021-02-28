71dc64074ee9d24afdcb6162119227d402846b61
Alexander <a.sribnyak8987@mail.ru>
2021-02-28 12:36:12 +0300

    Add the simplest version of DFT

--- app/src/main/java/com/sribnyak/thesound/MainActivity.kt
+++ app/src/main/java/com/sribnyak/thesound/MainActivity.kt
@@ -10,8 +10,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import kotlin.math.max
-import kotlin.math.roundToInt
+import kotlin.math.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -43,19 +42,49 @@ class MainActivity : AppCompatActivity() {
 
             val bufferSizeInBytes = max(
                 AudioRecord.getMinBufferSize(sampleRateInHz, channelConfig, audioFormat),
-                sampleRateInHz / DEFAULT_UPDATE_RATE * 2)
-            val bufferSizeInFrames = bufferSizeInBytes / 2
+                sampleRateInHz / DEFAULT_UPDATE_RATE * Short.SIZE_BYTES)
+            val bufferSizeInFrames = bufferSizeInBytes / Short.SIZE_BYTES
 
             val audioRecord = AudioRecord(MediaRecorder.AudioSource.MIC,
                 sampleRateInHz, channelConfig, audioFormat, bufferSizeInBytes)
             audioRecord.startRecording()
             val audioData = ShortArray(bufferSizeInFrames)
 
+            val ftRe = DoubleArray(bufferSizeInFrames)
+            val ftIm = DoubleArray(bufferSizeInFrames)
+            val ftResult = Array(bufferSizeInFrames / 2) { Pair(0, 0.0) }
             var volume: Int
+            var textToShow: CharSequence
             while (true) {
-                audioRecord.read(audioData, 0, audioData.size)
+                audioRecord.read(audioData, 0, bufferSizeInFrames)
+
+                for (k in ftRe.indices) {
+                    ftRe[k] = 0.0
+                    ftIm[k] = 0.0
+                    for (n in audioData.indices) {
+                        ftRe[k] += audioData[n] * cos(2 * PI * k * n / bufferSizeInFrames)
+                        ftIm[k] += audioData[n] * sin(2 * PI * k * n / bufferSizeInFrames)
+                    }
+                    ftRe[k] /= bufferSizeInFrames.toDouble()
+                    ftIm[k] /= bufferSizeInFrames.toDouble()
+                }
+                for (k in ftResult.indices) {
+                    ftResult[k] = Pair(
+                        (1.0 * k * sampleRateInHz / bufferSizeInFrames).roundToInt(),
+                        sqrt(ftRe[k] * ftRe[k] + ftIm[k] * ftIm[k])
+                    )
+                }
+                ftResult.sortByDescending { it.second }
+
                 volume = ((audioData.maxOrNull() ?: 0).toDouble() / Short.MAX_VALUE * 100).roundToInt()
-                textView.post { textView.text = "Permission granted!\n\nVolume: ${volume}%" }
+                textToShow = "Permission granted!\n\nVolume: ${volume}%\n\nPeaks:"
+                for (i in 0..4) {
+                    volume = (ftResult[i].second / Short.MAX_VALUE * 1000).roundToInt()
+                    if (volume < 1)
+                        break
+                    textToShow += "\n${ftResult[i].first} ($volume)"
+                }
+                textView.post { textView.text = textToShow }
             }
         }.start()
     }
@@ -71,6 +100,6 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val MIC_PERMISSION_CODE = 20567
         private const val DEFAULT_SAMPLE_RATE = 44100
-        private const val DEFAULT_UPDATE_RATE = 10
+        private const val DEFAULT_UPDATE_RATE = 30 // better 10
     }
 }
